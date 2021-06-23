a24c8d052eb409f3f2d75f2fec2f649c50541c62
leandroyabut <leandro.yabut@smoothstack.com>
2021-06-22 19:53:16 -0600

    Add user data model.

---
+++ src/main/java/com/arjay07/userservice/model/User.java
@@ -0,0 +1,18 @@
+package com.arjay07.userservice.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    private String username;
+    private String password;
+    private String email;
+    private String name;
+
+}
