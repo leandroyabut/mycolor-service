bee9d4f9a93bad6f17143693ee42e09190af0386
leandroyabut <leandro.yabut@smoothstack.com>
2021-06-22 20:10:29 -0600

    Update user model.

--- src/main/java/com/arjay07/userservice/model/User.java
+++ src/main/java/com/arjay07/userservice/model/User.java
@@ -1,12 +1,19 @@
 package com.arjay07.userservice.model;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class User {
 
     @Id
@@ -15,4 +22,16 @@ public class User {
     private String email;
     private String name;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return username.equals(user.username) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, email);
+    }
 }
