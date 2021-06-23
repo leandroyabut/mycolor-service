feac74b0383e28cdcdadb136dd83b47b42b15637
leandroyabut <leandro.yabut@smoothstack.com>
2021-06-22 20:12:40 -0600

    Add user service.

--- src/main/java/com/arjay07/userservice/repository/UserRepository.java
+++ src/main/java/com/arjay07/userservice/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.arjay07.userservice.repository;
 
 import com.arjay07.userservice.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, String> {
 }
---
+++ src/main/java/com/arjay07/userservice/service/UserService.java
@@ -0,0 +1,7 @@
+package com.arjay07.userservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
