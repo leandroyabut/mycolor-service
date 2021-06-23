950978d7a26fcaa3703c41c3f2f876f8e49609ed
leandroyabut <leandro.yabut@smoothstack.com>
2021-06-23 01:08:09 -0600

    Add app test.

---
+++ src/test/java/com/arjay07/mycolorservice/controller/UserControllerTest.java
@@ -0,0 +1,21 @@
+package com.arjay07.mycolorservice.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest
+class UserControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void test_getUserByUsername_status_is_ok_when_user_exists() {
+
+    }
+}
