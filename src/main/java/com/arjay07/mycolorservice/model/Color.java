ec06822d51fb562f97d366ab0aebcd7091388f58
leandroyabut <leandro.yabut@smoothstack.com>
2021-06-23 21:21:33 -0600

    Add color object.

---
+++ src/main/java/com/arjay07/mycolorservice/model/Color.java
@@ -0,0 +1,42 @@
+package com.arjay07.mycolorservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Color {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String hex;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Color color = (Color) o;
+        return id.equals(color.id) && hex.equals(color.hex);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, hex);
+    }
+}
